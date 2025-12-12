0714c8466bc6ed9d4759e0acb9819dd4c9614500
Vladislav Kochenkov <priznavologia@gmail.com>
2025-12-12 09:53:01 +0300

    Прорабатываю UI и HUD

--- Assets/_project/CodeBase/Enemies/LootSpawner.cs
+++ Assets/_project/CodeBase/Enemies/LootSpawner.cs
@@ -1,11 +1,6 @@
 ﻿using CodeBase.Data;
 using CodeBase.Infrastructure.Factory;
 using CodeBase.Infrastructure.Services;
-using CodeBase.Infrastructure.Services.PersistentProgress;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 namespace CodeBase.Enemies
@@ -50,125 +45,6 @@ namespace CodeBase.Enemies
         {
             _lootMin = min;
             _lootMax = max;
-        }
-    }
-
-    public class LootPiece : MonoBehaviour, ISavedProgress
-    {
-        public GameObject Skull;
-        public GameObject PickupFxPrefab;
-        public TextMeshPro LootText;
-        public GameObject PickupPopup;
-
-        private Loot _loot;
-        private bool _picked;
-        private WorldData _worldData;
-        [SerializeField] private string _id;
-
-        private IGameFactory _gameFactory;
-
-        public void Construct(WorldData worldData, IGameFactory gameFactory)
-        {
-            _worldData = worldData;
-            _gameFactory = gameFactory;
-        }
-
-        public void Initialize(Loot loot)
-        {
-            SetId(GenerateId());
-            _loot = loot;
-        }
-
-        private void OnTriggerEnter(Collider other) =>
-            PickUp();
-
-        public void SetId(string id) => _id = id;
-
-        public void UpdateProgress(PlayerProgress progress)
-        {
-            if (!CheckContainsId(progress))
-            {
-                LootObject lootObject = new(_id, transform.position, _loot);
-                progress.WorldData.LootData.LootsOnGround.Dict.Add(_id, lootObject);
-            }
-        }
-
-        public void LoadProgress(PlayerProgress progress)
-        {
-            _worldData = progress.WorldData;
-
-            if (TryGetLootPiece(progress, out LootObject lootPiece))
-            {
-                LoadData(lootPiece);
-                progress.WorldData.LootData.LootsOnGround.Dict.Remove(_id);
-            }
-        }
-
-        private bool CheckContainsId(PlayerProgress progress)
-        {
-            return GetLootDataDict(progress).ContainsKey(_id);
-        }
-
-        private bool TryGetLootPiece(PlayerProgress progress, out LootObject lootPiece)
-        {
-            return GetLootDataDict(progress).TryGetValue(_id, out lootPiece);
-        }
-
-        private static Dictionary<string, LootObject> GetLootDataDict(PlayerProgress progress)
-        {
-            return progress.WorldData.LootData.LootsOnGround.Dict;
-        }
-
-        private void LoadData(LootObject lootObject)
-        {
-            _loot = lootObject.loot;
-            transform.position = lootObject.positionData.AsUnityVector();
-            _id = lootObject.id;
-        }
-
-        private string GenerateId()
-        {
-            return $"{gameObject.scene.name}_{Guid.NewGuid().ToString()}";
-        }
-
-        private void PickUp()
-        {
-            if (_picked)
-                return;
-
-            _picked = true;
-
-            _gameFactory.ProgressWriters.Remove(this);
-            _gameFactory.ProgressReaders.Remove(this);
-
-            UpdateWorldData();
-            HideSkull();
-            PlayPickupFx();
-            Showtext();
-
-            StartCoroutine(StartDestroyTimer());
-        }
-
-        private void UpdateWorldData() =>
-            _worldData.LootData.Collect(_loot);
-
-        private void HideSkull() =>
-            Skull.SetActive(false);
-
-        private void PlayPickupFx() =>
-            Instantiate(PickupFxPrefab, transform.position, Quaternion.identity);
-
-        private void Showtext()
-        {
-            LootText.text = $"{_loot.Value}";
-            PickupPopup.SetActive(true);
-        }
-
-        private IEnumerator StartDestroyTimer()
-        {
-            yield return new WaitForSeconds(1.5f);
-
-            Destroy(gameObject);
         }
     }
 }
