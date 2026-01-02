db46a1c1b1d68ec1a04428914c587bd6c50a4314
Vladislav Kochenkov <priznavologia@gmail.com>
2026-01-02 10:24:51 +0300

    синхронизация веток

---
+++ Assets/_project/CodeBase/Infrastructure/Services/ObjectPool/PoolService.cs
@@ -0,0 +1,42 @@
+﻿using CodeBase.UI.Windows;
+using System;
+using System.Collections.Generic;
+
+namespace CodeBase.Infrastructure.Services.ObjectPool
+{
+    public class PoolService: IService
+    {
+        private Dictionary<Type, IPool> _pools;
+        
+        public PoolService()
+        {
+            _pools = new Dictionary<Type, IPool>
+            {
+                [typeof(Pool<WindowBase>)] = new Pool<WindowBase>(),
+            };
+        }
+
+        public IPool GetPool<T>() where T : IPool
+        {
+            return _pools[typeof(T)];
+        }
+    }
+
+    public interface IPool
+    {
+
+    }
+
+    public interface IPoolable
+    {
+        void OnSpawned();
+        void OnDespawned();
+    }
+
+    public class Pool<T>:IPool 
+       // where T : Component, IPoolable
+    {
+        readonly T prefab;
+        readonly Stack<T> freeObjects = new();
+    }
+}
