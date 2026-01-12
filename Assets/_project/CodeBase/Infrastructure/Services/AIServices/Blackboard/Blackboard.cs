4f9346358153fa5e9df77430a6db75077d95fa92
Vladislav Kochenkov <priznavologia@gmail.com>
2026-01-12 10:41:47 +0300

    Добавил класс хранения данных и класс Editor

--- Assets/_project/CodeBase/Infrastructure/Services/AIServices/Blackboard/Blackboard.cs
+++ Assets/_project/CodeBase/Infrastructure/Services/AIServices/Blackboard/Blackboard.cs
@@ -1,9 +1,6 @@
 ﻿using CodeBase.Infrastructure.Services.AIServices.Blackboard.BlackboardExtensions;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CodeBase.Infrastructure.Services.AIServices.Blackboard
 {
