9ae7441138260635b2ee88f1924464da13b3e9a0
Vladislav Kochenkov <priznavologia@gmail.com>
2026-01-12 10:34:01 +0300

    Логика BlackBoard

---
+++ Assets/_project/CodeBase/Infrastructure/Services/AIServices/Blackboard/Blackboard.cs
@@ -0,0 +1,76 @@
+﻿using CodeBase.Infrastructure.Services.AIServices.Blackboard.BlackboardExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBase.Infrastructure.Services.AIServices.Blackboard
+{
+    [Serializable]
+    public class Blackboard
+    {
+        private readonly Dictionary<string, BlackboardKey> _keyRegistry = new();
+        private readonly Dictionary<BlackboardKey, object> _entries = new();
+
+        public List<Action> PassedActions { get; } = new();
+
+        public void AddAction(Action action)
+        {
+            Preconditions.CheckNotNull(action);
+            PassedActions.Add(action);
+        }
+
+        public void ClearActions() => PassedActions.Clear();
+
+        public void Debug()
+        {
+            foreach (var entry in _entries)
+            {
+                var entryType = entry.Value.GetType();
+
+                if (entryType.IsGenericType && entryType.GetGenericTypeDefinition() == typeof(BlackboardEntry<>))
+                {
+                    var valueProperty = entryType.GetProperty("Value");
+                    if (valueProperty == null) continue;
+                    var value = valueProperty.GetValue(entry.Value);
+                    UnityEngine.Debug.Log($"Key: {entry.Key}, Value: {value}");
+                }
+            }
+        }
+
+        public bool TryGetValue<T>(BlackboardKey key, out T value)
+        {
+            if (_entries.TryGetValue(key, out var entry) && entry is BlackboardEntry<T> castedEntry)
+            {
+                value = castedEntry.Value;
+                return true;
+            }
+
+            value = default;
+            return false;
+        }
+
+        public void SetValue<T>(BlackboardKey key, T value)
+        {
+            _entries[key] = new BlackboardEntry<T>(key, value);
+        }
+
+        public BlackboardKey GetOrRegisterKey(string keyName)
+        {
+            Preconditions.CheckNotNull(keyName);
+
+            if (!_keyRegistry.TryGetValue(keyName, out BlackboardKey key))
+            {
+                key = new BlackboardKey(keyName);
+                _keyRegistry[keyName] = key;
+            }
+
+            return key;
+        }
+
+        public bool ContainsKey(BlackboardKey key) => _entries.ContainsKey(key);
+
+        public void Remove(BlackboardKey key) => _entries.Remove(key);
+    }
+}
---
+++ Assets/_project/CodeBase/Infrastructure/Services/AIServices/Blackboard/BlackboardKey.cs
@@ -0,0 +1,27 @@
+﻿using CodeBase.Infrastructure.Services.AIServices.Blackboard.BlackboardExtensions;
+using System;
+
+namespace CodeBase.Infrastructure.Services.AIServices.Blackboard
+{
+    [Serializable]
+    public readonly struct BlackboardKey : IEquatable<BlackboardKey>
+    {
+        private readonly string _name;
+        private readonly int _hashedKey;
+
+        public BlackboardKey(string name)
+        {
+            _name = name;
+            _hashedKey = name.ComputeFNV1aHash();
+        }
+
+        public bool Equals(BlackboardKey other) => _hashedKey == other._hashedKey;
+
+        public override bool Equals(object obj) => obj is BlackboardKey other && Equals(other);
+        public override int GetHashCode() => _hashedKey;
+        public override string ToString() => _name;
+
+        public static bool operator ==(BlackboardKey left, BlackboardKey right) => left._hashedKey == right._hashedKey;
+        public static bool operator !=(BlackboardKey left, BlackboardKey right) => !(left == right);
+    }
+}
