8804e05ee6920a9ffaa339066f1be98887c6043f
Vladislav Kochenkov <priznavologia@gmail.com>
2026-01-13 11:59:33 +0300

    Добавил методы расширения

---
+++ Assets/_project/CodeBase/Infrastructure/Services/AIServices/BehaviourTree/BehaviourTreeExtansions/BehaviourNodeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using CodeBase.Infrastructure.Services.AIServices.BehaviourTree.Core;
+
+namespace CodeBase.Infrastructure.Services.AIServices.BehaviourTree.BehaviourTreeExtansions
+{
+    public static class BehaviourNodeExtensions
+    {
+        /// <summary>
+        /// Добавляет ребёнка и возвращает сам узел, 
+        /// чтобы можно было строить дерево в цепочке.
+        /// </summary>
+        public static T Add<T>(this T node, BehaviourNode child) where T : BehaviourNode
+        {
+            node.Children.Add(child);
+            return node;
+        }
+    }
+}
