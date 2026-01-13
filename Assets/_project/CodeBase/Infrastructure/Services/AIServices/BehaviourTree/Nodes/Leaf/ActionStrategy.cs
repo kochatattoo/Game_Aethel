efa7f967d1934c537efe4e8d80986f36e0a0722c
Vladislav Kochenkov <priznavologia@gmail.com>
2026-01-13 12:31:05 +0300

    Добавил скрипт листьев

---
+++ Assets/_project/CodeBase/Infrastructure/Services/AIServices/BehaviourTree/Nodes/Leaf/ActionStrategy.cs
@@ -0,0 +1,21 @@
+﻿using CodeBase.Infrastructure.Services.AIServices.BehaviourTree.Core;
+using System;
+
+namespace CodeBase.Infrastructure.Services.AIServices.BehaviourTree.Nodes.Leaf
+{
+    public class ActionStrategy : IStrategy
+    {
+        readonly Action doSomething;
+
+        public ActionStrategy(Action doSomething)
+        {
+            this.doSomething = doSomething;
+        }
+
+        public BehaviourNode.Status Process()
+        {
+            doSomething();
+            return BehaviourNode.Status.Success;
+        }
+    }
+}
---
+++ Assets/_project/CodeBase/Infrastructure/Services/AIServices/BehaviourTree/Nodes/Leaf/WaitForCondition.cs
@@ -0,0 +1,21 @@
+﻿using CodeBase.Infrastructure.Services.AIServices.BehaviourTree.Core;
+using System;
+
+
+namespace CodeBase.Infrastructure.Services.AIServices.BehaviourTree.Nodes.Leaf
+{
+    public class WaitForCondition : IStrategy
+    {
+        private readonly Func<bool> _predicate;
+
+        public WaitForCondition(Func<bool> predicate)
+        {
+            _predicate = predicate;
+        }
+
+        public BehaviourNode.Status Process()
+        {
+            return _predicate() ? BehaviourNode.Status.Success : BehaviourNode.Status.Running;
+        }
+    }
+}
