aa9526ad887796a6783177388dd87f4fd8851da2
Vladislav Kochenkov <priznavologia@gmail.com>
2026-02-14 20:32:43 +0300

    Создал и забиндил пул ауйдиокомпонентов для дальнейшей логики управления воспроизведением звука

--- Assets/_project/CodeBase/Configs/AudioConfig.cs
+++ Assets/_project/CodeBase/Configs/AudioConfig.cs
@@ -2,8 +2,23 @@
 
 namespace CodeBase.Configs
 {
+    [CreateAssetMenu(fileName = nameof(AudioConfig), menuName = "StaticData/"+ nameof(AudioConfig))]
     public class AudioConfig: ScriptableObject
     {
+        [field: SerializeField]
+        public AudioClip Clip {  get; private set; }    
+        [field: SerializeField, Range(0, 1)] 
+        public float Volume { get; private set; }
+        [field: SerializeField, Range(0, 2)] 
+        public float PitchMin { get; private set; }
+        [field: SerializeField, Range(0, 2)] 
+        public float PitchMax { get; private set; }
 
+        public void ApplyTo(AudioSource source)
+        {
+            source.clip = Clip;
+            source.volume = Volume;
+            source.pitch = UnityEngine.Random.Range(PitchMin, PitchMax);
+        }
     }
 }
