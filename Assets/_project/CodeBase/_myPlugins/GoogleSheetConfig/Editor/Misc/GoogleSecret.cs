497256dee8a69a198d978960ac78900568a63351
Vladislav Kochenkov <priznavologia@gmail.com>
2026-06-11 08:52:37 +0300

    update git ignore

---
+++ Assets/_project/CodeBase/_myPlugins/GoogleSheetConfig/Editor/Misc/GoogleSecret.cs
@@ -0,0 +1,8 @@
+﻿namespace GoogleSheetConfig
+{
+    public class GoogleSecret
+    {
+        public static string CLIENT_ID { get; internal set; }
+        public static string CLIENT_SECRET { get; internal set; }
+    }
+}
